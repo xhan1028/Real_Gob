d9d0b3a9dfb5e4eda09ebc83d8c16c00399bad64
junmo908 <127921521+junmo908@users.noreply.github.com>
2023-06-12 14:51:56 +0900

    보스전 미완성 (플레이어 움직임과 점프 추가, 보스 패턴 구현중)

--- Assets/Scripts/BossScene/BossCtrl.cs
+++ Assets/Scripts/BossScene/BossCtrl.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class BossCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*private int nextPattern = 0;
+
+    private static readonly int NONE = 0;  
+    private static readonly int RUSH = 1;
+    private static readonly int LASER = 2;
+    private static readonly int JUMP = 3;
+    private static readonly int SPIT = 4;
+
+
+
+    IEnumerator rush()
     {
-        
+
+        nextPattern = Random.Range(1, 5);
+        nextPatternPlay()
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator laser()
     {
-        
+        nextPattern = Random.Range(1, 5);
+        nextPatternPlay()
     }
+
+    IEnumerator jump()
+    {
+        nextPattern = Random.Range(1, 5);
+        nextPatternPlay()
+    }
+
+    IEnumerator spit()
+    {
+        nextPattern = Random.Range(1, 5);
+        nextPatternPlay()
+    }
+
+    void Start()
+    {
+        StartCoroutine("rush");
+    }
+
+    void nextPatternPlay()
+    {
+        switch (nextPattern)
+        {
+            case 1:
+                StartCoroutine(rush());
+                break;
+            case 2:
+                StartCoroutine(laser());
+                break;
+            case 3:
+                StartCoroutine(jump());
+                break;
+            case 4:
+                StartCoroutine(spit());
+                break;
+        }
+    } */
 }
