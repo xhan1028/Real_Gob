c5241dceb0785966469a950ef62babf29e9f198c
junmo908 <127921521+junmo908@users.noreply.github.com>
2023-06-19 12:56:31 +0900

    플레이어 점프 수정

--- Assets/Scripts/BossScene/BossSkill.cs
+++ Assets/Scripts/BossScene/BossSkill.cs
@@ -2,40 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossCtrl : MonoBehaviour
+public class BossSkill : MonoBehaviour
 {
-    /*private int nextPattern = 0;
+    /*{
+    private int nextPattern = 0;
 
-    private static readonly int NONE = 0;  
+    private static readonly int NONE = 0;
     private static readonly int RUSH = 1;
-    private static readonly int LASER = 2;
+    private static readonly int EAT = 2;
     private static readonly int JUMP = 3;
     private static readonly int SPIT = 4;
 
-
-
     IEnumerator rush()
     {
-
-        nextPattern = Random.Range(1, 5);
+        
         nextPatternPlay()
     }
 
-    IEnumerator laser()
+    IEnumerator eat()
     {
-        nextPattern = Random.Range(1, 5);
+        
         nextPatternPlay()
     }
 
     IEnumerator jump()
     {
-        nextPattern = Random.Range(1, 5);
+        
         nextPatternPlay()
     }
 
     IEnumerator spit()
     {
-        nextPattern = Random.Range(1, 5);
+        
         nextPatternPlay()
     }
 
@@ -52,7 +50,7 @@ public class BossCtrl : MonoBehaviour
                 StartCoroutine(rush());
                 break;
             case 2:
-                StartCoroutine(laser());
+                StartCoroutine(eat());
                 break;
             case 3:
                 StartCoroutine(jump());
@@ -61,5 +59,5 @@ public class BossCtrl : MonoBehaviour
                 StartCoroutine(spit());
                 break;
         }
-    } */
+    }*/
 }
--- Assets/Scripts/BossScene/PlayerMove.cs
+++ Assets/Scripts/BossScene/PlayerMove.cs
@@ -7,18 +7,26 @@ public class PlayerMove : MonoBehaviour
     public float maxSpeed;
     public float jumpPower;
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+    bool isJump = false;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isJump /*&& !anim.GetBool("isJumping")*/)
         {
+            isJump = true;
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            //anim.SetBool("isJumping", true);
         }
+
         if (Input.GetButtonUp("Horizontal"))
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
@@ -48,10 +56,19 @@ public class PlayerMove : MonoBehaviour
             {
                 if (rayHit.distance < 0.5f)
                 {
-
+                    anim.SetBool("isJumping", false);
                 }
             }*/
         }
         
     }
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJump = false; 
+        }
+    }
+
 }
