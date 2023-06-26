c1fcc89aa57f543f0547688bd56a9bf52bee9a8b
xhan1028 <127803431+xhan1028@users.noreply.github.com>
2023-06-27 02:53:24 +0900

    512521

---
+++ Assets/Scripts/Load/Start_Binghaa.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Binghaa : MonoBehaviour
+{
+    public void backtitle()
+    {
+        SceneManager.LoadScene("Boss_3");
+    }
+}
